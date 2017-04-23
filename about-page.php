205e5c32f8768ce083cc808de34d3a6f323b9eb4
Louise Whitaker <l.whitaker@umiami.edu>
2017-04-22 20:00:03 -0400

    page intro template created

--- about-page.php
+++ about-page.php
@@ -5,28 +5,7 @@
 ?>
 
 
-
-<div class="header-image-wrapper">
-  <?php $image = get_field('page_hero_image');
-  if( !empty($image) ): ?>
-    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-  <?php endif; ?>
-  <div class="header-overlay"></div>
-</div>
-
-<div class="breadcrumbs-wrapper">
-  <div class="container">
-    <div class="breadcrumbs">
-      <?php get_breadcrumb(); ?>
-    </div>
-  </div>
-</div>
-
-<div class="container">
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
-  <?php endwhile; ?>
-</div>
+<?php get_template_part('templates/page', 'intro'); ?>
 
 <div class="container">
   <div class="row about_cards">
